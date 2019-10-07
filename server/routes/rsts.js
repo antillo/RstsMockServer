709aa382aacb4075af91064f2d019e77980363d8
Antonio Jesus Sanchez Colmenero <asanccol@everis.com>
2019-10-07 22:28:52 +0200

    Ordenacion archivos

---
+++ server/routes/rsts.js
@@ -0,0 +1,9 @@
+const expres = require('express');
+const app = express();
+
+
+app.post('/rsts');
+
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
 require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
