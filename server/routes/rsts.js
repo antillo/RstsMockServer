031b28ef7256882550377b95cbe5fe27ad42ea48
Antonio Jesus Sanchez Colmenero <asanccol@everis.com>
2019-10-08 08:09:43 +0200

    ordenacion codigos

--- server/routes/rsts.js
+++ server/routes/rsts.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.post('/rsts', function(req,res){
+app.post('/api/rsts', function(req,res){
     let body = req.body;
    
     res.json({
--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require( 'mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+
 app.use(require('./routes/rsts'));
 
 app.listen(process.env.PORT,()=>{console.log('Escuchando puerto',process.env.PORT)});
